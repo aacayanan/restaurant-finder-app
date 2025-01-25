3114120fc81db3c67b0f5fec5d1f7fc6b0b1c9e4
Aaron Cayanan <aaroncayanan02@gmail.com>
2025-01-25 14:17:35 -0800

    Finish SearchBar Component

---
+++ rose-hack/src/components/SearchBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {LucideMapPin} from "lucide-react";
+import PropTypes from 'prop-types';
+
+SearchBar.propTypes = {
+
+};
+
+function SearchBar(props) {
+    return (
+        <div className='flex-col justify-items-center'>
+            {/*text label*/}
+            <div className='flex justify-center text-sm py-2'>
+                Start by entering your location
+            </div>
+            <div className="flex p-3 border-2 border-gray-200 rounded-3xl justify-between">
+                <input type="text" placeholder="Enter Location"
+                       className="justify-self-center outline-none"/>
+                <LucideMapPin/>
+            </div>
+        </div>
+    );
+}
+
+export default SearchBar;
