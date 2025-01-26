8652ebd7e078b3eb6f677f569d4476c2dc9e1f75
Aaron Cayanan <aaroncayanan02@gmail.com>
2025-01-25 21:39:43 -0800

    Create FilterCard file

---
+++ rose-hack/src/components/FilterCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+FilterCard.propTypes = {
+
+};
+
+function FilterCard(props) {
+    return (
+        <div className="flex border-2 border-gray-700 rounded-3xl">
+
+        </div>
+    );
+}
+
+export default FilterCard;
